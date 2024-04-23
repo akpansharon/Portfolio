a0c01668b62e5aaeedfee1da4fbb42147fa02543
akpansharon <167903986+akpansharon@users.noreply.github.com>
2024-04-23 14:38:39 -0400

    Update and rename Exercise1 to Exercise1.py

---
+++ Python/Exercise1.py
@@ -0,0 +1,75 @@
+print("Hello World")
+print("Hello Python World")
+message = "Hello World!"
+print(message)
+
+message1 ="Hellow Python Crash Course"
+print(message1)
+
+#This is the module variables
+message2='Well you are doing it '
+print(message2)
+message3 = 'time to get grinding'
+print (message3)
+
+#This is module strings
+'this is a string'
+"this is also a string"
+'I tolf my friends, "Python is my favourite language!"'
+"The only language 'Python' is name after Month Python, not the snake"
+"One of Python's strenghts is its diverse and supportive community"
+
+#Case changing 
+name ="sharon akpan"
+print(name.title())
+
+#Uppercase  
+name1 ="Sharon Akpan"
+print(name1.upper())
+
+#Lowercase  
+name2 ="Sharon Akpan"
+print(name2.lower())
+
+first_name = 'sharon'
+last_name = 'akpan'
+full_name = f'{first_name} {last_name}'
+message = f"Hello, {full_name.title()}"
+print(message)
+
+#to add tab to the text
+print("Python")
+print("\tPython")
+print("Languages:\nPython\nC\nJavaScript")
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
+
+#stripping right white space
+favourite_language = 'python '
+favourite_language.rstrip()
+favourite_language = favourite_language.rstrip()
+
+#stripping left white space
+favourite_language1 = 'python '
+favourite_language1.lstrip()
+favourite_language1 = favourite_language1.lstrip()
+
+#removing prefixes
+nostarch_url = 'https://nostarch.com'
+nostarch_url.removeprefix('https://')
+simple_url = nostarch_url.removeprefix('https://')
+print (simple_url)
+
+#apostrophe
+message1 = "one of python's strenghts is its diverse community"
+print(message1)
+
+message2 = 'one of pythons strenghts is its diverse community'
+print(message2)
+
+
+
+
+
+
+
+
